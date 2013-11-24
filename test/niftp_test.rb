78da2132574ddb0f5621d5ce8b646bb70f1499f5
Christopher R. Murphy <chmurph2+git@gmail.com>
2013-11-24 15:21:15 -0500

    Mostly converts minitest to minispec syntax.
    * Had some trouble with must_raise so punted on it for now.
    * Replaces before block with a calls to `let`.

--- test/niftp_test.rb
+++ test/niftp_test.rb
@@ -4,112 +4,110 @@ require File.expand_path('../../lib/niftp', __FILE__)
 module NiFTP
   class NiFTPTest < TestCase
     describe "An object mixing in the NiFTP module" do
-      before do
-        @object = Object.new.extend NiFTP
-        @options = {}
-        @host = "localhost"
-        @ftp = stub_everything
-      end
+
+      let(:object) { Object.new.extend NiFTP }
+      let(:host)   { "localhost" }
+      let(:ftp)    { stub_everything }
 
       it "must be accessible via the Niftp shortcut" do
-        assert_equal Niftp, NiFTP
+        Niftp.must_equal NiFTP
       end
 
       it "must raise a runtime errors if the :tries option is less than 1" do
-        assert_raises(RuntimeError) { @object.ftp(@host, { :tries => 0 }) }
+        assert_raises(RuntimeError) { object.ftp(host, { :tries => 0 }) }
       end
 
       it "must connect to the FTP server with the default port" do
         stub_ftp
-        @ftp.expects(:connect).with(@host, 21).once.returns(stub_everything)
-        @object.ftp(@host)
+        ftp.expects(:connect).with(host, 21).once.returns(stub_everything)
+        object.ftp(host)
       end
 
       it "must connect to the FTP server with the optional port" do
         stub_ftp
-        @ftp.expects(:connect).with(@host, 2121).once.returns(stub_everything)
-        @object.ftp(@host, { :port => 2121 })
+        ftp.expects(:connect).with(host, 2121).once.returns(stub_everything)
+        object.ftp(host, { :port => 2121 })
       end
 
       it "must login to the FTP server with the default username and password" do
         stub_ftp
-        @ftp.expects(:login).with("", "")
-        @object.ftp(@host)
+        ftp.expects(:login).with("", "")
+        object.ftp(host)
       end
 
       it "must login to the FTP server with the optional username" do
         stub_ftp
-        @ftp.expects(:login).with("anonymous", "")
-        @object.ftp(@host, { :username => "anonymous" })
+        ftp.expects(:login).with("anonymous", "")
+        object.ftp(host, { :username => "anonymous" })
       end
 
       it "must login to the FTP server with the optional password" do
         stub_ftp
-        @ftp.expects(:login).with("", "some password")
-        @object.ftp(@host, { :password => "some password" })
+        ftp.expects(:login).with("", "some password")
+        object.ftp(host, { :password => "some password" })
       end
 
       it "must execute any arbitrary FTP code (i.e. the block)" do
         stub_ftp
         block_output = nil
-        @object.ftp(@host) do |ftp|
+        object.ftp(host) do |ftp|
           block_output = "block was executed"
         end
-        assert_equal "block was executed", block_output
+        "block was executed".must_equal block_output
       end
 
       it "must close the FTP connection" do
         stub_ftp
-        @ftp.expects(:close).once
-        @object.ftp(@host)
+        ftp.expects(:close).once
+        object.ftp(host)
       end
 
       it "must use the retryable defauls when they're not explicitly set" do
-        Net::FTP.stubs(:new => @ftp)
-        @object.expects(:retryable).with(:tries => 2, :sleep => 1,
+        Net::FTP.stubs(:new => ftp)
+        object.expects(:retryable).with(:tries => 2, :sleep => 1,
           :on => StandardError, :matching => /.*/)
-        @object.ftp(@host) { }
+        object.ftp(host) { }
       end
 
       it "must use the :tries option instead of the default" do
-        Net::FTP.expects(:new).times(3).returns(@ftp)
+        Net::FTP.expects(:new).times(3).returns(ftp)
         assert_raises(RuntimeError) do
-          @object.ftp(@host, {:tries => 3 }) do |ftp_client|
+          object.ftp(host, {:tries => 3 }) do |ftp_client|
             raise "testing retryable gem"
           end
         end
       end
 
       it "must close the FTP connection when there is an exception" do
-        Net::FTP.expects(:new).times(2).returns(@ftp)
-        @ftp.expects(:close).times(2)
+        Net::FTP.expects(:new).times(2).returns(ftp)
+        ftp.expects(:close).times(2)
         assert_raises(RuntimeError) do
-          @object.ftp(@host) { |ftp_client| raise "testing close"}
+          object.ftp(host) { |ftp_client| raise "testing close"}
         end
       end
 
       it "must use a five second (default) timeout when connecting" do
         stub_ftp
         Timeout.expects(:timeout).with(30)
-        @object.ftp(@host)
+        object.ftp(host)
       end
 
       it "must use the :timeout option instead of the default" do
         stub_ftp
         Timeout.expects(:timeout).with(1)
-        @object.ftp(@host, { :timeout => 1 })
+        object.ftp(host, { :timeout => 1 })
       end
 
       it "must use the FTPFXP (FTPS) library when instructed" do
-        Net::FTPFXPTLS.expects(:new).once.returns(@ftp)
-        @object.ftp(@host, options = { :ftps => true })
+        Net::FTPFXPTLS.expects(:new).once.returns(ftp)
+        object.ftp(host, { :ftps => true })
       end
     end
 
     private
 
     def stub_ftp
-      Net::FTP.stubs(:new => @ftp)
+      Net::FTP.stubs(:new => ftp)
     end
   end
 end
